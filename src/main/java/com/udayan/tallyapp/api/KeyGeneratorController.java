cdad0672981a5ef31e1f477396c8a922f1d63a9f
Biswajit Debnath <biswajit.sust@gmail.com>
2025-05-19 11:15:33 +0600

    key generator controller param added

--- src/main/java/com/udayan/tallyapp/api/KeyGeneratorController.java
+++ src/main/java/com/udayan/tallyapp/api/KeyGeneratorController.java
@@ -4,6 +4,7 @@ import com.udayan.tallyapp.utils.Utils;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,22 +12,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class KeyGeneratorController {
 
     @GetMapping("/otp")
-    public ResponseEntity<?> otp() {
-        return ResponseEntity.ok(Utils.generateOTP(6));
+    public ResponseEntity<?> otp(@RequestParam(name = "length", defaultValue = "6", required = false) Integer length) {
+        return ResponseEntity.ok(Utils.generateOTP(length));
     }
 
     @GetMapping("/secret-key")
-    public ResponseEntity<?> secretKey() {
-        return ResponseEntity.ok(Utils.generateSecretKey(64));
+    public ResponseEntity<?> secretKey(@RequestParam(name = "length", defaultValue = "64", required = false) Integer length) {
+        return ResponseEntity.ok(Utils.generateSecretKey(length));
     }
 
     @GetMapping("/salt")
-    public ResponseEntity<?> salt() {
-        return ResponseEntity.ok(Utils.generateSalt(32));
+    public ResponseEntity<?> salt(@RequestParam(name = "length", defaultValue = "32", required = false) Integer length) {
+        return ResponseEntity.ok(Utils.generateSalt(length));
     }
 
     @GetMapping("/activation-key")
-    public ResponseEntity<?> activationKey() {
-        return ResponseEntity.ok(Utils.generateActivationCodeKey(32));
+    public ResponseEntity<?> activationKey(@RequestParam(name = "length", defaultValue = "32", required = false) Integer length) {
+        return ResponseEntity.ok(Utils.generateActivationCodeKey(length));
     }
 }
